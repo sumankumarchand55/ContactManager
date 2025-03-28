e97f98c3fe664e7be3edcf0574a387d1610b243a
SUMAN KUMAR CHAND <chandsumankumar@gmail.com>
2025-03-28 20:00:10 +0000

    test

--- src/main/java/com/smart/contact/SmartcontactmanagerApplication.java
+++ src/main/java/com/smart/contact/SmartcontactmanagerApplication.java
@@ -2,8 +2,10 @@ package com.smart.contact;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SmartcontactmanagerApplication {
 
 	public static void main(String[] args) {
